403fed786adf99fca8e03116d96e40e7f948bd4f
MauriceMens <mauricemensah5@gmail.com>
2026-06-23 19:24:44 +0000

    Update polynomes.h

--- Somme_Polynomes/polynomes.h
+++ Somme_Polynomes/polynomes.h
@@ -5,52 +5,52 @@
    STRUCTURES DE DONNÉES DU PROJET
 ======================================================== */
 
-typedef struct {
+typedef struct TMonome TMonome{
     int coeff;
     int expo;
-} TData;
+};
 
-typedef struct Element {
+typedef struct TElement TElement{
     TData data;
     struct Element *suivant;
-} TElement;
+};
 
-typedef struct {
+typedef struct TListe TListe {
     TElement *debut;
     TElement *fin;
     int taille;
-} TListe;
+};
 
 /* ========================================================
    REPARTITION DES 9 ROLES DU GROUPE
 ======================================================== */
 
 // 👑 TON RÔLE : Gestion structurelle de base
-void InitialiserListe(TListe *L);
+void InitialiserListe(TListe *liste);
 
 // 👥 RÔLE 2 : Insertion d'un monôme
-void AjouterElement(TListe *L, int coeff, int expo);
+void AjouterElement(TListe *liste, int coeff, int expo);
 
 // 👥 RÔLE 3 : Nettoyage de la mémoire
-void LibererListe(TListe *L);
+void LibererListe(TListe *liste);
 
 // 👥 RÔLE 4 : Sauvegarde dans un fichier binaire
-void Sauvegarder(TListe *L, char *nomFichier);
+void Sauvegarder(TListe *liste, char *nomFichier);
 
 // 👥 RÔLE 5 : Chargement depuis un fichier binaire
-void Charger(TListe *L, char *nomFichier);
+void Charger(TListe *liste, char *nomFichier);
 
 // 👥 RÔLE 6 : Algorithme de calcul (Somme / Soustraction)
-void Calculer(TListe *P1, TListe *P2, TListe *R, int signe);
+void Calculer(TListe *polynome1, TListe *polynome2, TListe *R, int signe);
 
 // 👥 RÔLE 7 : Affichage console mis en forme
-void Afficher(TListe *L, char *nom);
+void Afficher(TListe *liste, char *nom);
 
 // 👥 RÔLE 8 : Interface utilisateur et saisie de P1
 void AfficherMenuPrincipal();
-void SaisirPolynome1(TListe *P1);
+void SaisirPolynome1(TListe *polynome1);
 
 // 👥 RÔLE 9 : Saisie de P2 et Gestion de la boucle principale
-void SaisirPolynome2(TListe *P2);
+void SaisirPolynome2(TListe *polynome2);
 
 #endif // POLYNOMES_H
